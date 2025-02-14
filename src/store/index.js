2d38a10cc86d8af74dd83213268cdbc3407cf89e
oluwamuibiclear <igbokweudoka@gmail.com>
2025-02-13 22:11:17 -0600

    index reducer

---
+++ src/store/index.js
@@ -0,0 +1,28 @@
+import { createStore } from "redux";
+
+const counterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === "increment") {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+  if (action.type === "decrement") {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+  return state;
+};
+const store = redux.createStore(counterReducer);
+
+// console.log(store.getState());
+
+counterSubscriber = () => {
+  const latestState = store.getState();
+
+  console.log(latestState);
+};
+
+store.subscribe(counterSubscriber);
+store.dispatch({ type: "decrement" });
+store.dispatch({ type: "increment" });
