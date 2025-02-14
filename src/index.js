adbc194332a46fdb1aa4954dfd749fb5debb0350
oluwamuibiclear <igbokweudoka@gmail.com>
2025-02-13 22:14:27 -0600

    import provider and store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+import { Provider } from "react-redux";
+import store from "./store/index";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
