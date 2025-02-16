9b009623a53626de3a1710d8a6ed02a9169101d6
oluwamuibiclear <igbokweudoka@gmail.com>
2025-02-15 20:19:22 -0600

    show

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -4,6 +4,7 @@ import { useSelector, useDispatch } from "react-redux";
 const Counter = () => {
   const dispatch = useDispatch();
   const counter = useSelector((state) => state.counter);
+  const show = useSelector((state) => state.showCounter);
   const incrementHandler = () => {
     dispatch({ type: "increment" });
   };
