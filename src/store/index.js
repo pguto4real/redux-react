5816375622c7146e81370c2beb86a3179633859c
oluwamuibiclear <igbokweudoka@gmail.com>
2025-02-15 20:39:42 -0600

    counterSlice variable

--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = { counter: 0, showCounter: true };
 
-createSlice({
+const counterSlice = createSlice({
   name: "counter",
 
   initialState,
