24fc0ae56eedfac904173cc1b83ad3b03c61f015
oluwamuibiclear <igbokweudoka@gmail.com>
2025-02-15 21:03:10 -0600

    clean up

--- src/store/index.js
+++ src/store/index.js
@@ -3,45 +3,9 @@ import { createStore } from "redux";
 import { configureStore } from "@reduxjs/toolkit";
 import counterSlice from "./counterSlice";
 
-// const counterReducer = (state = initialState, action) => {
-//   if (action.type === "increment") {
-//     return {
-//       counter: state.counter + 1,
-//       showCounter: state.showCounter,
-//     };
-//   }
-//   if (action.type === "decrement") {
-//     return {
-//       counter: state.counter - 1,
-//       showCounter: state.showCounter,
-//     };
-//   }
-//   if (action.type === "increase") {
-//     return {
-//       counter: state.counter + action.amount,
-//       showCounter: state.showCounter,
-//     };
-//   }
-//   if (action.type === "toggle") {
-//     return {
-//       counter: state.counter,
-//       showCounter: !state.showCounter,
-//     };
-//   }
-//   return state;
-// };
 const store = configureStore({
   reducer: counterSlice,
 });
 
-// console.log(store.getState());
 export default store;
-// counterSubscriber = () => {
-//   const latestState = store.getState();
 
-//   console.log(latestState);
-// };
-
-// store.subscribe(counterSubscriber);
-// store.dispatch({ type: "decrement" });
-// store.dispatch({ type: "increment" });
