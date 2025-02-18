817e5ccdd0d80850b20b10c0bf5bca885cd9baff
oluwamuibiclear <igbokweudoka@gmail.com>
2025-02-17 19:40:54 -0600

    fix

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,9 @@
-import { createStore } from "redux";
-
 import { configureStore } from "@reduxjs/toolkit";
 import counterSlice from "./counterSlice";
 import authSlice from "./authSlice";
 
 const store = configureStore({
-  reducer: { counter: counterSlice, auth: authSlice },
+  reducer: { ui: counterSlice, auth: authSlice },
 });
 
 export default store;
